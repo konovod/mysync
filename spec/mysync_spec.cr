29f2f1a140ea36159331ead76402e6e7e88ab48a
Konovod <konovod@gmail.com>
2017-02-17 23:03:52 -0100

    more sanity to specs

--- spec/mysync_spec.cr
+++ spec/mysync_spec.cr
@@ -26,27 +26,35 @@ struct TestServerOutput
 
 end
 
+class SpecLogger
+  @@events = [] of String
 
-def log_srv(s : String)
-  puts "SERVER: #{s}"
-end
+  def self.log_srv(s : String)
+    @@events << "SERVER: #{s}"
+  end
 
-def log_cli(s : String)
-  puts "CLIENT: #{s}"
+  def self.log_cli(s : String)
+    @@events << "CLIENT: #{s}"
+  end
+
+  def self.dump_events
+    r = @@events.clone
+    @@events.clear
+    r
+  end
+
 end
 
 class TestUserContext < MySync::UserContext(TestClientInput, TestServerOutput)
 
   def on_disconnect
-    log_srv "user disconnected: #{@user}"
+    SpecLogger.log_srv "user disconnected: #{@user}"
   end
   def on_received_sync
-    log_srv "received: #{@remote_sync}"
-    #@server.state.all_data[@remote_sync.num] = @remote_sync.str if @remote_sync.num >= 0
+    @server.state.all_data[@remote_sync.num] = @remote_sync.data if @remote_sync.num >= 0
   end
   def before_sending_sync
     @local_sync = @server.state
-    log_srv "sending: #{@local_sync}"
   end
 
   def initialize(aserver, auser)
@@ -59,12 +67,12 @@ class TestServer < MySync::Server(TestClientInput, TestServerOutput)
   property state = TestServerOutput.new
 
   def on_login(user)
-    log_srv "logged in: #{user}"
+    SpecLogger.log_srv "logged in: #{user}"
     return TestUserContext.new(self, user)
   end
 
   def on_register(user)
-    log_srv "registered: #{user}"
+    SpecLogger.log_srv "registered: #{user}"
     return TestUserContext.new(self, user)
   end
 
@@ -74,11 +82,11 @@ end
 class TestClient < MySync::Client(TestClientInput, TestServerOutput)
 
   def on_connected(user : MySync::UserID)
-    log_cli "logged in: #{user}"
+    SpecLogger.log_cli "logged in: #{user}"
   end
 
   def on_received_sync
-    log_cli "received"
+    SpecLogger.log_cli "received"
   end
 
   def before_sending_sync
@@ -97,14 +105,19 @@ describe "basic client\server" do
   cli.on_connected(2)
 
   it "works" do
-
+    SpecLogger.dump_events.should eq ["SERVER: logged in: 2", "CLIENT: logged in: 2"]
   end
 
   it "parse packets when passed directly" do
+    cli.local_sync.data = "hello"
+    cli.local_sync.num = 5
+
     n = cli.process_sending
     srv_inst.package_received.copy_from(cli.package_tosend.to_unsafe, n)
     srv_inst.process_receive
+    srv.state.all_data[5].should eq "hello"
   end
+
 
 
 end
