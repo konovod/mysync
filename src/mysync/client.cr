2d66406f76b8e05430acd9d5fb99eab58d1540ec
Konovod <konovod@gmail.com>
2017-02-24 21:09:24 -0100

    reading fiber at client

--- src/mysync/client.cr
+++ src/mysync/client.cr
@@ -11,37 +11,49 @@ module MySync
     def initialize(@address : Address, @endpoint : AbstractEndPoint)
       @socket = UDPSocket.new
       @socket.connect @address
-      @received = Package.new(MAX_PACKAGE_SIZE)
+      @raw_received = Bytes.new(MAX_PACKAGE_SIZE)
       @received_decrypted = Package.new(MAX_PACKAGE_SIZE)
       @tosend = Package.new(MAX_PACKAGE_SIZE)
       @nonce = Crypto::Nonce.new
       @symmetric_key = Crypto::SymmetricKey.new
+      @header = @raw_received.to_unsafe.as(UInt32*)
+      spawn { reading_fiber }
     end
 
     # TODO - send packages asynchronously?
-    private def package_received
+    private def package_received(package : Bytes)
       # first it decrypts and check
-      return if @received.size <= Crypto::OVERHEAD_SYMMETRIC
-      @received_decrypted.size = @received.size - Crypto::OVERHEAD_SYMMETRIC
-      return unless Crypto.symmetric_decrypt(key: @symmetric_key, input: @received.slice, output: @received_decrypted.slice)
+      return if package.size <= Crypto::OVERHEAD_SYMMETRIC
+      @received_decrypted.size = package.size - Crypto::OVERHEAD_SYMMETRIC
+      return unless Crypto.symmetric_decrypt(key: @symmetric_key, input: package, output: @received_decrypted.slice)
       # then pass to endpoint
       @endpoint.process_receive(@received_decrypted.slice)
     end
-  end
 
-  def send_data
-    data = @endpoint.process_sending
-    # then encrypt
-    @nonce.reroll
-    @tosend.size = data.size + Crypto::OVERHEAD_SYMMETRIC
-    Crypto.symmetric_encrypt(key: @symmetric_key, nonce: @nonce, input: data, output: @tosend)
-    # then send back
-    begin
-      @socket.send(@tosend, @address)
-    rescue ex : Errno
-      if ex.errno == Errno::ECONNREFUSED
-        # well, message didn't pass
-        p ex.inspect
+    private def reading_fiber
+      loop do
+        size, ip = @socket.receive(@raw_received)
+        next if size < 4
+        next if size > MAX_PACKAGE_SIZE
+        next if @header.value != RIGHT_SIGN
+        package_received @raw_received[4, size - 4]
+      end
+    end
+
+    def send_data
+      data = @endpoint.process_sending
+      # then encrypt
+      @nonce.reroll
+      @tosend.size = data.size + Crypto::OVERHEAD_SYMMETRIC
+      Crypto.symmetric_encrypt(key: @symmetric_key, nonce: @nonce, input: data, output: @tosend)
+      # then send back
+      begin
+        @socket.send(@tosend, @address)
+      rescue ex : Errno
+        if ex.errno == Errno::ECONNREFUSED
+          # well, message didn't pass
+          p ex.inspect
+        end
       end
     end
   end
