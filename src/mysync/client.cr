743913cb4e5ab9864aed82c56195f9a988d29acf
Konovod <konovod@gmail.com>
2017-02-24 21:03:06 -0100

    updated client.cr to match interface changes

--- src/mysync/client.cr
+++ src/mysync/client.cr
@@ -6,35 +6,35 @@ require "./package"
 
 module MySync
   class Client
-    getter package
     property symmetric_key
 
     def initialize(@address : Address, @endpoint : AbstractEndPoint)
       @socket = UDPSocket.new
       @socket.connect @address
-      @package = Package.new(MAX_PACKAGE_SIZE)
-      @package_decrypted = Package.new(MAX_PACKAGE_SIZE)
+      @received = Package.new(MAX_PACKAGE_SIZE)
+      @received_decrypted = Package.new(MAX_PACKAGE_SIZE)
+      @tosend = Package.new(MAX_PACKAGE_SIZE)
       @nonce = Crypto::Nonce.new
       @symmetric_key = Crypto::SymmetricKey.new
     end
 
     # TODO - send packages asynchronously?
-    def package_received
+    private def package_received
       # first it decrypts and check
-      return if @package.size <= Crypto::OVERHEAD_SYMMETRIC
-      @package_decrypted.size = @package.size - Crypto::OVERHEAD_SYMMETRIC
-      return unless Crypto.symmetric_decrypt(key: @symmetric_key, input: @package.slice, output: @package_decrypted)
+      return if @received.size <= Crypto::OVERHEAD_SYMMETRIC
+      @received_decrypted.size = @received.size - Crypto::OVERHEAD_SYMMETRIC
+      return unless Crypto.symmetric_decrypt(key: @symmetric_key, input: @received.slice, output: @received_decrypted.slice)
       # then pass to endpoint
-      @endpoint.process_receive
+      @endpoint.process_receive(@received_decrypted.slice)
     end
   end
 
   def send_data
-    n = @endpoint.process_sending
-    return if n <= 0
+    data = @endpoint.process_sending
     # then encrypt
     @nonce.reroll
-    Crypto.symmetric_encrypt(key: @symmetric_key, nonce: @nonce, input: @package_decrypted, output: @package)
+    @tosend.size = data.size + Crypto::OVERHEAD_SYMMETRIC
+    Crypto.symmetric_encrypt(key: @symmetric_key, nonce: @nonce, input: data, output: @tosend)
     # then send back
     begin
       @socket.send(@tosend, @address)
