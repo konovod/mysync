82f68acc4d8b645f3a5f5e4cdab7ad267cb189d1
Konovod <konovod@gmail.com>
2017-05-07 00:33:36 +0000

    wip debugging, refactored auth parts of client to client_auth

--- src/mysync/client.cr
+++ src/mysync/client.cr
@@ -5,31 +5,21 @@ require "./network"
 require "./utils/package"
 require "./payloads/rpc"
 require "./payloads/lists"
+require "./client_auth"
 
 module MySync
-  enum AuthState
-    NoData
-    SendingLogin
-    SendingPass
-    LoginFailed
-    LoggedIn
-
-    def restartable?
-      {SendingLogin, SendingPass, LoggedIn}.includes? self
-    end
-  end
-
   class UDPGameClient
     getter socket
     getter rpc_manager
-    getter auth_state
     property debug_loss = false
     property autosend_delay : TimeDelta?
     property autologin_delay : TimeDelta?
     property disconnect_timeout : TimeDelta
-    @server_key : Crypto::PublicKey?
-    @login_pass : (Crypto::SecretKey | String | Nil) = nil
     @time : TimeProvider
+
+    def debug_str(string)
+      p string
+    end
 
     def initialize(@endpoint : EndPoint, @address : Address)
       @socket = UDPSocket.new
@@ -46,28 +36,15 @@ module MySync
       @time = @endpoint.time
       @login_key = Crypto::SymmetricKey.new
       @symmetric_key = Crypto::SymmetricKey.new
-      @auth_state = AuthState::NoData
       @autosend_delay = nil
       @autologin_delay = nil
       @should_send = Channel(Nil).new
       @disconnect_timeout = TimeDelta.new(1*SECOND)
       @last_response = Time.new(0)
 
-      @login_name = ""
-      @login_salt = Crypto::Salt.new
-
       spawn { reading_fiber }
       spawn { sending_fiber }
       spawn { timed_fiber }
-    end
-
-    def save_hash
-      case @login_pass
-      when Crypto::SecretKey
-        return @login_pass
-      else
-        return nil
-      end
     end
 
     private def package_received(package : Bytes)
@@ -95,22 +72,31 @@ module MySync
       end
     end
 
+    GOOD_SIGN = {
+      AuthState::SendingLogin => RIGHT_LOGIN_SIGN,
+      AuthState::SendingPass  => RIGHT_PASS_SIGN,
+      AuthState::LoggedIn     => RIGHT_SIGN,
+    }
     private def reading_fiber
       loop do
         size, ip = try_receive
         next if size < MIN_RAW_SIZE
         next if size > MAX_RAW_SIZE
         package = @raw_received[4, size - 4]
-        # p "recv #{@auth_state}"
+        unless GOOD_SIGN[@auth_state] == @received_header.value
+          debug_str "cli recv failed: #{@auth_state} => #{@received_header.value} need #{GOOD_SIGN[@auth_state]}"
+          return
+        end
+        debug_str "cli recv #{@auth_state} "
         case @auth_state
         when AuthState::SendingLogin
-          login_received(package) if @received_header.value == RIGHT_LOGIN_SIGN
+          login_received(package)
         when AuthState::SendingPass
-          pass_received(package) if @received_header.value == RIGHT_PASS_SIGN
+          pass_received(package)
         when AuthState::LoggedIn
-          package_received (package) if @received_header.value == RIGHT_SIGN
+          package_received (package)
         else
-          # p "failed #{@auth_state}"
+          debug_str "cli failed #{@auth_state}"
           # ignored
         end
       end
@@ -122,7 +108,7 @@ module MySync
         if @time.current - @last_response > @disconnect_timeout && @auth_state.restartable?
           @auth_state = AuthState::SendingLogin
         end
-        # p "sending #{@auth_state}"
+        debug_str "cli sending #{@auth_state}"
         case @auth_state
         when AuthState::SendingLogin
           send_login
@@ -159,107 +145,6 @@ module MySync
 
     def send_manually
       @should_send.send nil
-    end
-
-    def login(public_key : Crypto::PublicKey, alogin : String, pass) : Nil
-      @server_key = public_key
-      @login_name = alogin
-      @login_pass = pass
-      @auth_state = AuthState::SendingLogin
-    end
-
-    def wait_login : Bool
-      raise "autologin_delay should be set" unless @autologin_delay
-      @auth_state = MySync::AuthState::SendingLogin
-      # send_manually
-      loop do
-        Fiber.yield
-        return true if @auth_state == MySync::AuthState::LoggedIn
-        return false if @auth_state == MySync::AuthState::LoginFailed
-      end
-    end
-
-    private def pass_received(package : Bytes)
-      # first it decrypts and check
-      return if package.size <= Crypto::OVERHEAD_SYMMETRIC
-      @received_decrypted.size = package.size - Crypto::OVERHEAD_SYMMETRIC
-      return unless Crypto.decrypt(key: @symmetric_key, input: package, output: @received_decrypted.slice)
-      @last_response = @time.current
-      if @received_decrypted.slice[0] == 1
-        # all is fine, start listening
-        # data = Bytes.new(@received_decrypted.size - Crypto::SymmetricKey.size - 1)
-        # data.copy_from @received_decrypted.slice[1 + Crypto::SymmetricKey.size, data.size]
-        @auth_state = AuthState::LoggedIn
-        @endpoint.reset
-      else
-        # auth failed with a reason
-        # data = Bytes.new(@received_decrypted.size - 1)
-        # data.copy_from @received_decrypted.slice[1, data.size]
-        @auth_state = AuthState::LoginFailed
-      end
-    end
-
-    private def send_login
-      secret_key = Crypto::SecretKey.new
-      @login_key = Crypto::SymmetricKey.new(our_secret: secret_key, their_public: @server_key.not_nil!)
-      our_public = Crypto::PublicKey.new(secret: secret_key)
-      # we encrypt auth data and add our public key as additional data
-      @tosend.size = 4 + Crypto::PublicKey.size + Crypto::OVERHEAD_SYMMETRIC + @login_name.bytes.size
-      @tosend.slice[4, Crypto::PublicKey.size].copy_from our_public.to_slice
-      Crypto.encrypt(
-        key: @login_key,
-        input: @login_name.to_slice,
-        output: @tosend.slice[4 + Crypto::PublicKey.size, @login_name.size + Crypto::OVERHEAD_SYMMETRIC])
-      # send it to server
-      @tosend_header.value = RIGHT_LOGIN_SIGN
-      try_send
-    end
-
-    private def send_pass
-      pass_data = @login_pass
-      case pass_data
-      when Crypto::SecretKey
-        data = pass_data.to_slice
-      when String
-        # p "starting KDF"
-        key = Crypto::SecretKey.new(password: pass_data, salt: @login_salt)
-        # p "KDF done"
-        @login_pass = key
-        data = key.to_slice
-        # as it takes too much time anyway
-        # @auth_state = AuthState::SendingLogin
-        # send_manually
-        # return
-      else
-        raise "no pass info provided"
-      end
-      # then encrypt
-      @tosend.size = data.size + Crypto::OVERHEAD_SYMMETRIC + 4
-      Crypto.encrypt(key: @symmetric_key, input: data, output: @tosend.slice[4, @tosend.size - 4])
-      # then send back
-      @tosend_header.value = RIGHT_PASS_SIGN
-      try_send
-    end
-
-    def login_received(package) : Nil
-      # decrypt it with symmetric_key
-      return if package.size < Crypto::OVERHEAD_SYMMETRIC
-      @received_decrypted.size = package.size - Crypto::OVERHEAD_SYMMETRIC
-      return unless Crypto.decrypt(key: @login_key, input: package, output: @received_decrypted.slice)
-      @last_response = @time.current
-      if @received_decrypted.slice[0] == 1
-        # all is fine, copy symmetric_key and data to output and start listening
-        @login_key.reroll
-        @symmetric_key.to_slice.copy_from @received_decrypted.slice[1, Crypto::SymmetricKey.size]
-        @login_salt = Crypto::Salt.from_bytes @received_decrypted.slice[1 + Crypto::SymmetricKey.size, Crypto::Salt.size]
-        @auth_state = AuthState::SendingPass
-        send_manually
-      else
-        # auth failed with a reason
-        # data = Bytes.new(@received_decrypted.size - 1)
-        # data.copy_from @received_decrypted.slice[1, data.size]
-        @auth_state = AuthState::LoginFailed
-      end
     end
 
     private def send_data
---
+++ src/mysync/client_auth.cr
@@ -0,0 +1,144 @@
+require "monocypher"
+require "socket"
+require "./endpoint"
+require "./network"
+require "./utils/package"
+require "./payloads/rpc"
+require "./payloads/lists"
+
+module MySync
+  enum AuthState
+    NoData
+    SendingLogin
+    SendingPass
+    LoginFailed
+    LoggedIn
+
+    def restartable?
+      {SendingLogin, SendingPass, LoggedIn}.includes? self
+    end
+  end
+
+  class UDPGameClient
+    getter auth_state = AuthState::NoData
+    @server_key : Crypto::PublicKey?
+    @login_pass : (Crypto::SecretKey | String | Nil) = nil
+    @login_name = ""
+    @login_salt = Crypto::Salt.new
+
+    def save_hash
+      case @login_pass
+      when Crypto::SecretKey
+        return @login_pass
+      else
+        return nil
+      end
+    end
+
+    def login(public_key : Crypto::PublicKey, alogin : String, pass) : Nil
+      @server_key = public_key
+      @login_name = alogin
+      @login_pass = pass
+      @auth_state = AuthState::SendingLogin
+    end
+
+    def wait_login : Bool
+      raise "autologin_delay should be set" unless @autologin_delay
+      @auth_state = MySync::AuthState::SendingLogin
+      # send_manually
+      loop do
+        Fiber.yield
+        return true if @auth_state == MySync::AuthState::LoggedIn
+        return false if @auth_state == MySync::AuthState::LoginFailed
+      end
+    end
+
+    private def send_login
+      secret_key = Crypto::SecretKey.new
+      @login_key = Crypto::SymmetricKey.new(our_secret: secret_key, their_public: @server_key.not_nil!)
+      our_public = Crypto::PublicKey.new(secret: secret_key)
+      # we encrypt auth data and add our public key as additional data
+      @tosend.size = 4 + Crypto::PublicKey.size + Crypto::OVERHEAD_SYMMETRIC + @login_name.bytes.size
+      @tosend.slice[4, Crypto::PublicKey.size].copy_from our_public.to_slice
+      Crypto.encrypt(
+        key: @login_key,
+        input: @login_name.to_slice,
+        output: @tosend.slice[4 + Crypto::PublicKey.size, @login_name.size + Crypto::OVERHEAD_SYMMETRIC])
+      # send it to server
+      @tosend_header.value = RIGHT_LOGIN_SIGN
+      try_send
+    end
+
+    private def send_pass
+      pass_data = @login_pass
+      case pass_data
+      when Crypto::SecretKey
+        data = pass_data.to_slice
+      when String
+        debug_str "starting KDF"
+        key = Crypto::SecretKey.new(password: pass_data, salt: @login_salt)
+        debug_str "KDF done"
+        @login_pass = key
+        data = key.to_slice
+        # as it takes too much time anyway
+        # @auth_state = AuthState::SendingLogin
+        # send_manually
+        # return
+      else
+        raise "no pass info provided"
+      end
+      # then encrypt
+      @tosend.size = data.size + Crypto::OVERHEAD_SYMMETRIC + 4
+      Crypto.encrypt(key: @symmetric_key, input: data, output: @tosend.slice[4, @tosend.size - 4])
+      # then send back
+      @tosend_header.value = RIGHT_PASS_SIGN
+      try_send
+    end
+
+    private def pass_received(package : Bytes)
+      # first it decrypts and check
+      p "step1"
+      return if package.size <= Crypto::OVERHEAD_SYMMETRIC
+      p "step2"
+      @received_decrypted.size = package.size - Crypto::OVERHEAD_SYMMETRIC
+      return unless Crypto.decrypt(key: @symmetric_key, input: package, output: @received_decrypted.slice)
+      p "step3"
+      debug_str "pass response: #{@received_decrypted.slice[0]}"
+      @last_response = @time.current
+      if @received_decrypted.slice[0] == 1
+        # all is fine, start listening
+        # data = Bytes.new(@received_decrypted.size - Crypto::SymmetricKey.size - 1)
+        # data.copy_from @received_decrypted.slice[1 + Crypto::SymmetricKey.size, data.size]
+        @auth_state = AuthState::LoggedIn
+        @endpoint.reset
+      else
+        # auth failed with a reason
+        # data = Bytes.new(@received_decrypted.size - 1)
+        # data.copy_from @received_decrypted.slice[1, data.size]
+        @auth_state = AuthState::LoginFailed
+      end
+    end
+
+    def login_received(package) : Nil
+      # decrypt it with symmetric_key
+      return if package.size < Crypto::OVERHEAD_SYMMETRIC
+      @received_decrypted.size = package.size - Crypto::OVERHEAD_SYMMETRIC
+      return unless Crypto.decrypt(key: @login_key, input: package, output: @received_decrypted.slice)
+      @last_response = @time.current
+      debug_str "login response: #{@received_decrypted.slice[0]}"
+      if @received_decrypted.slice[0] == 1
+        # all is fine, copy symmetric_key and data to output and start listening
+        @login_key.reroll
+        @symmetric_key.to_slice.copy_from @received_decrypted.slice[1, Crypto::SymmetricKey.size]
+        @login_salt = Crypto::Salt.from_bytes @received_decrypted.slice[1 + Crypto::SymmetricKey.size, Crypto::Salt.size]
+        @auth_state = AuthState::SendingPass
+        send_manually
+      else
+        # auth failed with a reason
+        # data = Bytes.new(@received_decrypted.size - 1)
+        # data.copy_from @received_decrypted.slice[1, data.size]
+        @auth_state = AuthState::LoginFailed
+      end
+    end
+  end
+end
